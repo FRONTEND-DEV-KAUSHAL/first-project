854f8cdbaa493cc21baa68c4fedbc05dfee234cf
FRONTEND-DEV-KAUSHAL <gohilkaushal16@gmail.com>
2022-06-03 19:37:23 +0530

    city

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import Demo from './componant/Demo';
+import City from './containers/City';
 
 function App() {
   return (
-    <Demo/>
+    // <Demo/>
+    <City/>
   );
 }
 
--- src/containers/city.js
+++ src/containers/city.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
-class city extends Component {
+class City extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                CityName : 'surat'
+            }
+    }
+    
+    Change_City = () => {
+        this.setState = {
+            CityName : 'amreli'
+        };
+    }
     render() {
         return (
             <div>
-                <p></p>
+                <p>{this.CityName}</p>
+                <button>Change city</button>
             </div>
         );
     }
 }
 
-export default city;
+export default City;
