e69f691673f00d67a1f587cfdb3dd799f7cf9416
FRONTEND-DEV-KAUSHAL <you@exampl>
2022-06-03 05:30:11 -0700

    rnw-object

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const myObj = {
-    name: "John",
-    age: 30,
-    cars: {
-        car1: "Ford",
-        car2: "BMW",
-        car3: "Fiat"
+  const data = {
+    personal_info: {
+        name: 'amit',
+        age: 25,
+        city: 'surat'
+    },
+    courses: ["C", "JavaScript", "React"],
+    branches: {
+        rwl: {
+            admission: 50,
+            vacant_seat: 10
+        },
+        rw2: {
+            admission: 30,
+            vacant_seat: 20
+        },
+        rw3: {
+            admission: 25,
+            vacant_seat: 25
+        },
+        rw4: {
+            admission: 40,
+            vacant_seat: 10
+        }
     }
-}
-for (let k in myObj) {
-  if (k == "age") {
-    
-    console.log(myObj[k]);
-  }
-
 }
   return 
 
