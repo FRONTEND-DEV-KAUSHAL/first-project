8063ed0fcd1ebb0bdca68a956d10ecc1d544bc4f
FRONTEND-DEV-KAUSHAL <gohilkaushal16@gmail.com>
2022-06-03 12:10:49 +0530

    employee data

--- src/App.js
+++ src/App.js
@@ -2,76 +2,77 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let medicine_data =
-      [
-        {
-          id: 101,
-          name: 'Abacavir',
-          quantity: 25,
-          price: 150,
-          expiry: 2022,
-          status: true
-        },
-        {
-          id: 102,
-          name: 'Eltrombopag',
-          quantity: 90,
-          price: 550,
-          expiry: 2021,
-          status: true
-        },
-        {
-          id: 103,
-          name: 'Meloxicam',
-          quantity: 85,
-          price: 450,
-          expiry: 2025,
-          status: false
-        },
-        {
-          id: 104,
-          name: 'Allopurinol',
-          quantity: 50,
-          price: 600,
-          expiry: 2023,
-          status: true
-        },
-        {
-          id: 105,
-          name: 'Phenytoin',
-          quantity: 63,
-          price: 250,
-          expiry: 2021,
-          status: false
-        }
-      ];
-      let filterdata = medicine_data.filter((d,i) => d.status === true && d.expiry >= 2022)
-      console.log(filterdata);
-      let total = filterdata.reduce((acc ,d ,i) => acc+d.price,0);
-      console.log(total);
+  let data = [
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ];
+  let fdata = data.filter((d,i) => d.status === true);
+  let rdata = fdata.reduce((acc, d, i) => acc+d.bonus+d.salary,0);
+  console.log(rdata);
   return (
-    <table border="1">
+    <table border="1" align="center">
       <tr>
-        <td>id</td>
         <td>name</td>
-        <td>quantity</td>
-        <td>price</td>
-        <td>expiry</td>
+        <td>age</td>
+        <td>salary</td>
+        <td>bonus</td>
         <td>status</td>
         <td>total</td>
-      </tr>
+      </tr> 
       {
-        filterdata.map((d,i) =>{
-          let {id, name, quantity, price, expiry, status} = d;
+        fdata.map((d,i) =>{
+          let {name, age, salary, bonus, status} = d;
           return(
-            <tr>
-              <td>{id}</td>
+            <tr key={i}>
               <td>{name}</td>
-              <td>{quantity}</td>
-              <td>{price}</td>
-              <td>{expiry}</td>
+              <td>{age}</td>
+              <td>{salary}</td>
+              <td>{bonus}</td>
               <td>{status.toString()}</td>
-              {i===0 ? <td rowSpan={2}>{total}</td> : null}
+              <td>{salary+bonus}</td>
+            </tr>
+          )
+        })
+      }
+      {
+        fdata.map((d,i) =>{
+          let {name, age, salary, bonus, status} = d;
+          return(
+            <tr key={i}>
+              {i===0 ? <td colSpan={6}>total expense is = {rdata}</td> : null}
             </tr>
           )
         })
