1a8846b8d571e5ff9e3dda554baa5ac3f0db94c0
FRONTEND-DEV-KAUSHAL <gohilkaushal16@gmail.com>
2022-06-08 11:13:26 +0530

    digital-watch with function

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import Time from './Containers/Time/Time.js';
+import TimeFun from './Containers/Time/TimeFun';
 
 
 function App() {
   return (
     // <Demo/>
     // <City/>
-    <Time/>
+    // <Time/>
+    <TimeFun />
   );
 }
 
--- src/containers/Time/TimeFun.js
+++ src/containers/Time/TimeFun.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TimeFun(props) {
-     
+
+    const [time, SetTime] = useState(new Date());
+
+    const tick = () => {
+        SetTime(new Date());
+    }
+    
+    useEffect(() => {
+        const timeI = setInterval(() => tick(), 1000);
+
+        return() => {
+            clearInterval(timeI);
+        }
+    },[time]);
     return (
         <div>
-            
+            <p>{time.toLocaleTimeString()}</p>
         </div>
     );
 }
